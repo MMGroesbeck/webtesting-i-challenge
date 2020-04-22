fbcab8dddd3b99acf84198ab236fbbb92190f569
M Groesbeck <m.groesbeck@gmail.com>
2020-04-22 14:27:05 -0700

    MVP met.

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,17 +6,24 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  return {
+    ...item,
+    enhancement: Math.min(item.enhancement + 1, 20),
+  };
 }
 
 function fail(item) {
-  return { ...item };
+  return {
+    ...item,
+    durability: item.durability - (item.enhancement < 15 ? 5 : 10),
+    enhancement: item.enhancement - (item.enhancement > 16 ? 1 : 0),
+  };
 }
 
 function repair(item) {
-  return{
+  return {
     ...item,
-    durability: 100
+    durability: 100,
   };
 }
 
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,29 +1,101 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
-describe("enhancer.js", function() {
-    it("should run all tests without error", () => {
-        expect(true).toBe(true);
+describe("enhancer.js", function () {
+  it("should run all tests without error", () => {
+    expect(true).toBe(true);
+  });
+  describe(".succeed(item)", () => {
+    it("should return new item with enhancement increased by 1 (max 20)", () => {
+      expect(
+        enhancer.succeed({
+          name: "flamberge",
+          durability: 25,
+          enhancement: 7,
+        })
+      ).toEqual({
+        name: "flamberge",
+        durability: 25,
+        enhancement: 8,
+      });
+      expect(
+        enhancer.succeed({
+          name: "fork of horripilation",
+          durability: 99,
+          enhancement: 20,
+        })
+      ).toEqual({
+        name: "fork of horripilation",
+        durability: 99,
+        enhancement: 20,
+      });
     });
-    describe(".succeed(item)", () => {
-        it.todo("succeed(item) should return a new item with the same info as item argument, but with item.enhancement increased by 1 (max 20)");
+  });
+  describe(".fail(item)", () => {
+    it("should return new item: enhancement < 15 -> durability -5; enhancement >= 15 -> durability -= 10; enhancement > 16 -> enhancement -= 1", () => {
+      expect(
+        enhancer.fail({
+          name: "voulge",
+          durability: 50,
+          enhancement: 5,
+        })
+      ).toEqual({
+        name: "voulge",
+        durability: 45,
+        enhancement: 5,
+      });
+      expect(
+        enhancer.fail({
+          name: "skillet",
+          durability: 77,
+          enhancement: 15,
+        })
+      ).toEqual({
+        name: "skillet",
+        durability: 67,
+        enhancement: 15,
+      });
+      expect(
+        enhancer.fail({
+          name: "MissingNo.",
+          durability: 80,
+          enhancement: 18,
+        })
+      ).toEqual({
+        name: "MissingNo.",
+        durability: 70,
+        enhancement: 17,
+      });
     });
-    describe(".fail(item)", () => {
-        it.todo(".fail(item) should return a new item. If item.enhancement < 15, item.durability -= 5; if item.enhancement >= 15, item.durability -= 10; if item.enhancement > 16, item.enhancement -= 1");
+  });
+  describe(".repair(item)", () => {
+    it("should return a new item with durability increased to 100", () => {
+      expect(
+        enhancer.repair({
+          name: "bardiche-guisarme",
+          durability: 73,
+          enhancement: 0,
+        })
+      ).toEqual({
+        name: "bardiche-guisarme",
+        durability: 100,
+        enhancement: 0,
+      });
+      expect(
+        enhancer.repair({
+          name: "spatula",
+          durability: 100,
+          enhancement: 13,
+        })
+      ).toEqual({
+        name: "spatula",
+        durability: 100,
+        enhancement: 13,
+      });
     });
-    describe(".repair(item)", () => {
-        it("should return a new item with durability increased to 100", () => {
-            expect(enhancer.repair({
-                name: "bardiche-guisarme",
-                durability: 73,
-                enhancement: 0
-            })).toEqual({
-                name: "bardiche-guisarme",
-                durability: 100,
-                enhancement: 0
-            });
-        });
-    });
-    describe(".get(item)", () => {
-        it.todo(".get(item) returns a new item with name edited to be same if enhancement = 0, [+<enhancement>] <name> if enhancement > 0");
-    });
+  });
+  describe(".get(item)", () => {
+    it.todo(
+      ".get(item) returns a new item with name edited to be same if enhancement = 0, [+<enhancement>] <name> if enhancement > 0"
+    );
+  });
 });
